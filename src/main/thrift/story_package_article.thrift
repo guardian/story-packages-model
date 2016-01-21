6957f3fb4efb2c5c39da61e6141289c09dae868e
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-01-21 08:33:39 +0000

    add image cutout src

--- src/main/thrift/story_package_article.thrift
+++ src/main/thrift/story_package_article.thrift
@@ -7,6 +7,11 @@ enum ArticleType {
     Snap = 2
 }
 
+enum Group {
+    Included = 1,
+    Linked = 2
+}
+
 /**
 * this stucture represents articles and its overrides
 **/
@@ -16,29 +21,31 @@ struct Article {
 
     2: required ArticleType articleType;
 
-    3: optional string headline;
+    3: required Group group
 
-    4: optional string href;
+    4: optional string headline;
 
-    5: optional string trailText;
+    5: optional string href;
 
-    6: optional string imageSrc;
+    6: optional string trailText;
 
-    7: optional bool isBoosted;
+    7: optional string imageSrc;
 
-    8: optional bool imageHide;
+    8: optional bool isBoosted;
 
-    9: optional bool showMainVideo;
+    9: optional bool imageHide;
 
-    10: optional bool showKickerTag;
+    10: optional bool showMainVideo;
 
-    11: optional bool showKickerSection;
+    11: optional bool showKickerTag;
 
-    12: optional string byline;
+    12: optional bool showKickerSection;
 
-    13: optional string customKicker;
+    13: optional string byline;
 
-    14: optional bool showBoostedHeadline;
+    14: optional string customKicker;
 
-    15: optional bool showQuotedHeadline;
+    15: optional bool showBoostedHeadline;
+
+    16: optional bool showQuotedHeadline;
 }
